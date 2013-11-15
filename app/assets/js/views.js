32cf666db2d74b8d7a582d0f23fed668a52bf077
Tim Dettrick <t.dettrick@uq.edu.au>
2013-11-15 13:20:23 +1000

    Javascript avatars.

--- app/assets/js/views.js
+++ app/assets/js/views.js
@@ -21,7 +21,7 @@ define([
   'use strict';
 
   var svgOrPng = Modernizr.svg ? 'svg' : 'png';
-
+  
   var toMarkdown = function(html) {
     return html2markdown(html, { parser: markdownDOMParser });
   };
@@ -424,7 +424,6 @@ define([
 
   var WatchingButtonView = FlagButtonView.extend({
     dataDefaults: function() {
-      
       return {
         icon: this.isSet() ? 'eye' : 'eye-slash',
         flagType: 'watch',
@@ -473,10 +472,26 @@ define([
     }
   });
 
-  var VersionView = Backbone.Marionette.ItemView.extend({
+  var UserAvatar = Backbone.Marionette.ItemView.extend({
+    tagName: 'span',
+    serializeData: function() {
+      return this.model.avatar(_(this.options).pick('missing', 'size'));
+    },
+    template: function(data) {
+      return templates.render('user_avatar', data);
+    },
+    onRender: function(data) {
+      this.$el.find('[rel=tooltip]').tooltip();
+    }
+  });
+
+  var VersionView = Backbone.Marionette.Layout.extend({
     tagName: 'tr',
     modelEvents: {
       "change": "render"
+    },
+    regions: {
+      avatar: '.user-avatar'
     },
     triggers: {
       "click .delete": "version:delete"
@@ -500,8 +515,18 @@ define([
     template: function(serialized_model) {
       return templates.render('version_row', serialized_model);
     },
+    avatarView: function() {
+      if (!this._avatar) {
+        this._avatar = new UserAvatar({
+          model: this.model,
+          size: 20
+        });
+      }
+      return this._avatar;
+    },
     onRender: function($e) {
       formatTimestamp(this.ui.timestamp);
+      this.avatar.show(this.avatarView())
     },
     onVersionDelete: function(e) {
       e.model.destroy();
@@ -1536,12 +1561,21 @@ define([
       this.addRegions({
         notifications: new Backbone.Marionette.Region({
           el: '#notifications-nav-item'
+        }),
+        currentUserAvatar: new Backbone.Marionette.Region({
+          el: '#current-user-avatar'
         })
       });
       this.notifications.show(new NotificationsNavView({
         collection: this.notificationMessages
       }));
       this.notificationMessages.fetch();
+      this.users.once('sync reset', function() {
+        this.currentUserAvatar.show(new UserAvatar({
+          model: this.users.current(),
+          size: 20
+        }));
+      }, this);
     },
     getFileTree: function() {
       if (_.isUndefined(this._fileTree)) {
